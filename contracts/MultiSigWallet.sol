f05f4d850d3cc5b3091ebc8749dbaf9df485d4ee
Yannis <JoeysinYao@gmail.com>
2023-10-26 02:10:16 +0800

    fix多签钱包

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -30,7 +30,7 @@ contract MultiSigWallet {
 
     // 函数修改器
     modifier onlyOwner() {
-        require(isOwner[msg.sender], "not owner");
+        require(isOwner[msg.sender], 'not owner');
         _;
     }
     modifier txExists(uint256 _txId) {
@@ -38,26 +38,23 @@ contract MultiSigWallet {
         _;
     }
     modifier notApproved(uint256 _txId) {
-        require(!approved[_txId][msg.sender], "tx already approved");
+        require(!approved[_txId][msg.sender], 'tx already approved');
         _;
     }
     modifier notExecuted(uint256 _txId) {
-        require(!transactions[_txId].exected, "tx is exected");
+        require(!transactions[_txId].exected, 'tx is exected');
         _;
     }
 
     // 构造函数
     constructor(address[] memory _owners, uint256 _required) {
-        require(_owners.length > 0, "owner required");
-        require(
-            _required > 0 && _required <= _owners.length,
-            "invalid required number of owners"
-        );
+        require(_owners.length > 0, 'owner required');
+        require(_required > 0 && _required <= _owners.length, 'invalid required number of owners');
 
         for (uint256 index = 0; index < _owners.length; index++) {
             address owner = _owners[index];
-            require(owner != address(0), "invalid owner");
-            require(!isOwner[owner], "owner is not unique"); // 如果重复会抛出错误
+            require(owner != address(0), 'invalid owner');
+            require(!isOwner[owner], 'owner is not unique'); // 如果重复会抛出错误
             isOwner[owner] = true;
             owners.push(owner);
         }
@@ -73,48 +70,32 @@ contract MultiSigWallet {
         address _to,
         uint256 _value,
         bytes calldata _data
-    ) external onlyOwner returns(uint256){
-        transactions.push(
-            Transaction({to: _to, value: _value, data: _data, exected: false})
-        );
+    ) external onlyOwner returns (uint256) {
+        transactions.push(Transaction({ to: _to, value: _value, data: _data, exected: false }));
         emit Submit(transactions.length - 1);
         return transactions.length - 1;
     }
 
-    function approv(uint256 _txId)
-        external
-        onlyOwner
-        txExists(_txId)
-        notApproved(_txId)
-        notExecuted(_txId)
-    {
+    function approv(
+        uint256 _txId
+    ) external onlyOwner txExists(_txId) notApproved(_txId) notExecuted(_txId) {
         approved[_txId][msg.sender] = true;
         emit Approve(msg.sender, _txId);
     }
 
-    function execute(uint256 _txId)
-        external
-        onlyOwner
-        txExists(_txId)
-        notExecuted(_txId)
-    {
-        require(getApprovalCount(_txId) >= required, "approvals < required");
+    function execute(uint256 _txId) external onlyOwner txExists(_txId) notExecuted(_txId) {
+        require(getApprovalCount(_txId) >= required, 'approvals < required');
 
         Transaction storage transaction = transactions[_txId];
         transaction.exected = true;
-
-        (bool sucess, ) = transaction.to.call{value: transaction.value}(
+        (bool success, ) = payable(transaction.to).call{ value: transaction.value }(
             transaction.data
         );
-        require(sucess, "tx failed");
+        require(success, 'Transfer failed.');
         emit Execute(_txId);
     }
 
-    function getApprovalCount(uint256 _txId)
-        public
-        view
-        returns (uint256 count)
-    {
+    function getApprovalCount(uint256 _txId) public view returns (uint256 count) {
         for (uint256 index = 0; index < owners.length; index++) {
             if (approved[_txId][owners[index]]) {
                 count += 1;
@@ -122,14 +103,13 @@ contract MultiSigWallet {
         }
     }
 
-    function revoke(uint256 _txId)
-        external
-        onlyOwner
-        txExists(_txId)
-        notExecuted(_txId)
-    {
-        require(approved[_txId][msg.sender], "tx not approved");
+    function revoke(uint256 _txId) external onlyOwner txExists(_txId) notExecuted(_txId) {
+        require(approved[_txId][msg.sender], 'tx not approved');
         approved[_txId][msg.sender] = false;
         emit Revoke(msg.sender, _txId);
+    }
+
+    function queryTran(uint32 _txId) external view returns (Transaction memory) {
+        return transactions[_txId];
     }
 }
